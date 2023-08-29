1228732949ed8b7d749a991e058585751d802ee0
tahaeddiani <Tahaeddiani2@gmail.com>
2023-08-29 12:55:38 -0700

    task13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -7,7 +7,7 @@
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 int num = 1;
-if ((tree->right == NULL && tree->left == NULL) || tree == NULL )
+if (tree == NULL || (tree->right == NULL && tree->left == NULL))
 	return (0);
 num += binary_tree_nodes(tree->right);
 num += binary_tree_nodes(tree->left);
